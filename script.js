4fbb74e609f73a3e9d7ded2f42b1212aed23dd5c
MikeProduce <Gomez_miguelomar@yahoo.com>
2022-08-27 21:39:32 -0500

    Update styles.css

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+const content1 = document.getElementById("container-content");
+const content2 = document.getElementById("container-content2");
+const leftbutton = document.getElementById("leftbutton");
+const rightbutton = document.getElementById("rightbutton");
+
+const eachslide = [content1, content2];
+
+const changeslides = function (n) {
+  for (let i = 0; i < eachslide.length; i++) {
+    rightbutton.onclick;
+  }
+};
