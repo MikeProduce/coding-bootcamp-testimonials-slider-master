6c595dc814c4554887cef3823443ee254e490ff2
MikeProduce <Gomez_miguelomar@yahoo.com>
2022-08-28 22:40:37 -0500

    trying to do slide show

--- script.js
+++ script.js
@@ -5,28 +5,3 @@ const leftbutton = document.getElementById("leftbutton");
 const rightbutton = document.getElementById("rightbutton");
 
 const eachslide = [content1, content2];
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-leftbutton.addEventListener("click", function plusSlides(n) {
-  showSlides((slideIndex += n));
-});
-
-function showSlides(n) {
-  const eachslide = [content1, content2];
-  let i;
-  let slides = eachslide;
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  slides[slideIndex - 1].style.display = "block";
-}
