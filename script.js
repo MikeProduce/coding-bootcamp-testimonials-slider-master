355cc66175bc8b1d6f8ce6bdb31d2c7faf018ef4
MikeProduce <Gomez_miguelomar@yahoo.com>
2022-08-29 08:04:28 -0500

    still cant figure out the loop for the slide show

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 "use strict";
-const content1 = document.getElementById("container-content");
+// const content1 = document.getElementById("container-content");
 const content2 = document.getElementById("container-content2");
 const leftbutton = document.getElementById("leftbutton");
 const rightbutton = document.getElementById("rightbutton");
 
-const eachslide = [content1, content2];
+// const eachslide = [content1, content2];
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides() {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  const content1 = document.getElementById("container-content");
+  if (n > content1.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = content1.length;
+  }
+  for (i = 0; i < content1.length; i++) {}
+}
