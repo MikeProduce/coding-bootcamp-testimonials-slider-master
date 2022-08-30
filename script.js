81429aa088b7bcac93c430b716c6cc95c7b04f99
MikeProduce <Gomez_miguelomar@yahoo.com>
2022-08-29 23:20:51 -0500

    uhm i dont know how i did it. but i did it?

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 "use strict";
-// const content1 = document.getElementById("container-content");
+const content1 = document.getElementById("container-content");
 const content2 = document.getElementById("container-content2");
 const leftbutton = document.getElementById("leftbutton");
 const rightbutton = document.getElementById("rightbutton");
 
 // const eachslide = [content1, content2];
 
-let slideIndex = 1;
-showSlides(slideIndex);
+var i = 0;
+var images = [];
 
-function plusSlides() {
-  showSlides((slideIndex += n));
-}
+images[1] = content1;
+images[2] = content2;
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  let i;
-  const content1 = document.getElementById("container-content");
-  if (n > content1.length) {
-    slideIndex = 1;
+var nextImage = function () {
+  if (i < images.length) {
+    images[i++];
+    images[i++].style.display = "block";
+    images[i++].style.display = "none";
+  } else {
+    images[i--];
+    images[i--].style.display = "block";
+    images[i--].style.display = "none";
   }
-  if (n < 1) {
-    slideIndex = content1.length;
+};
+
+var previousImage = function () {
+  if (i < images.length) {
+    images[i++];
+    images[i++].style.display = "block";
+    images[i++].style.display = "none";
+  } else {
+    images[i--];
+    images[i--].style.display = "block";
+    images[i--].style.display = "none";
   }
-  for (i = 0; i < content1.length; i++) {}
-}
+};
+rightbutton.addEventListener("click", nextImage);
+leftbutton.addEventListener("click", previousImage);
